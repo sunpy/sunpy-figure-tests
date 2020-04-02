c8a69eaca9f55e026f9f207b37b419b0ecf7c6d0
David Stansby <dstansby@gmail.com>
2020-04-02 16:18:39 +0100

    Parameterize tests

--- tests/test_hashes.py
+++ tests/test_hashes.py
@@ -8,18 +8,19 @@ import pytest
 from sunpy.tests import hash
 
 
-@pytest.fixture()
-def hashes():
-    hashfile = 'https://raw.githubusercontent.com/sunpy/sunpy/master/sunpy/tests/figure_hashes_py36.json'
-    hashfile = urllib.request.urlopen(hashfile)
-    return json.load(hashfile)
+hashfile = 'https://raw.githubusercontent.com/sunpy/sunpy/master/sunpy/tests/figure_hashes_py36.json'
+hashfile = urllib.request.urlopen(hashfile)
+hashes = json.load(hashfile)
 
 
-def test_hashes(hashes):
-    figpath = pathlib.Path(os.path.abspath(__file__)) / '..' / '..' / 'figures'
-    figpath = figpath.resolve()
-    figs = [x for x in figpath.iterdir() if x.suffix == '.png']
-    for fig in figs:
-        with open(fig, 'rb') as f:
-            fhash = hash._hash_file(f)
-        assert hashes[fig.stem] == fhash
+figpath = pathlib.Path(os.path.abspath(__file__)) / '..' / '..' / 'figures'
+figpath = figpath.resolve()
+figure_paths = [x for x in figpath.iterdir() if x.suffix == '.png']
+ids = [figure_path.name for figure_path in figure_paths]
+
+
+@pytest.mark.parametrize('fig_path', figure_paths, ids=ids)
+def test_hash(fig_path):
+    with open(fig_path, 'rb') as f:
+        fhash = hash._hash_file(f)
+    assert hashes[fig_path.stem] == fhash
