46ec5f89151f2152f663b8a5a4555b6522f3351e
David Stansby <dstansby@gmail.com>
2020-04-24 13:13:42 +0100

    Delete old figures and fix tests

--- tests/test_hashes.py
+++ tests/test_hashes.py
@@ -13,7 +13,7 @@ hashfile = urllib.request.urlopen(hashfile)
 hashes = json.load(hashfile)
 
 
-figpath = pathlib.Path(os.path.abspath(__file__)) / '..' / '..' / 'figures'
+figpath = pathlib.Path(os.path.abspath(__file__)) / '..' / '..' / 'figures' / 'figure_py36'
 figpath = figpath.resolve()
 figure_paths = [x for x in figpath.iterdir() if x.suffix == '.png']
 ids = [figure_path.name for figure_path in figure_paths]
